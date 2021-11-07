6b7f6707595f182a89db8455fbaebd4da7f4afa0
ksmmr <singo01052@icloud.com>
2021-11-07 15:56:12 +0900

    diary

--- testapp/urls.py
+++ testapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'testapp'
+
 urlpatterns = [
+    path("", views.index, name='index'),
     path('celery_test/', views.celery_test, name='celery_test'),
     path("boot/",views.boot,name="boot"),
+    path("add/", views.adddiary, name="add"),
 ]
 
--- testapp/views.py
+++ testapp/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from celery.result import AsyncResult
 from celeryTest.tasks import add
+from .forms import DayCreateForm
+from .models import Day
 # TaskResultをインポート
 from django_celery_results.models import TaskResult
-
-
 
 def celery_test(request):
 
@@ -16,9 +16,10 @@ def celery_test(request):
 
 	print('task_id:', task_id)
 
-	context = {"result":result.get(timeout=1),
+	context = {
 			   "ready":result.ready(),
-			   'taskid': task_id}
+			   'taskid': task_id,
+	}
 
 	return render(request, 'testapp/celery_test.html', context)
 
@@ -28,3 +29,24 @@ def boot(request):
 		"redis": os.environ["REDIS_URL"],
 	}
 	return render(request,"testapp/index.html",context)
+
+def index(request):
+	context = {
+        'day_list':Day.objects.all(),
+    }
+	return render(request,'testapp/day_list.html',context)
+
+def adddiary(request):
+    #送信内容を基にフォームをつくる
+    form = DayCreateForm(request.POST or None)
+
+    #method=POSTつまり、送信ボタン押下時、入力内容に問題がなければ
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect('testapp:index')
+
+    #通常ページのアクセス、入力内容に誤りがある場合、またページを表示
+    context = {
+        'form':form
+    }
+    return render(request,'testapp/day_form.html',context)
