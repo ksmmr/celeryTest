c163348fc49779afef08e7f8f4dbc77dcbebeb89
ksmmr <singo01052@icloud.com>
2021-11-07 10:24:50 +0900

    initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn celeryTest.wsgi --log-file -
---
+++ testapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from celery.result import AsyncResult
+
+from celeryTest.tasks import add
+
+def celery_test(request):
+	task_id = add.delay(5, 5)
+
+	result = AsyncResult(task_id)
+	print('result:', result, ' : ', result.state, ' : ', result.ready())
+
+	context = {'result': result}
+
+	return render(request, 'testapp/celery_test.html', context)
