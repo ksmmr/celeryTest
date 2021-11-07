bd3dc854683b3f8e03d6cc588ae2f38981bf2615
ksmmr <singo01052@icloud.com>
2021-11-07 11:25:15 +0900

    taskresult

--- testapp/views.py
+++ testapp/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from celery.result import AsyncResult
+from django_celery_results.models import TaskResult
 
 from celeryTest.tasks import add
 
 def celery_test(request):
+	"""
 	task_id = add.delay(5, 5)
 
 	result = AsyncResult(task_id)
 	print('result:', result, ' : ', result.state, ' : ', result.ready())
 
 	context = {'result': result}
+	"""
+	add.delay(1, 1)
+	object_list = TaskResult.objects.all().order_by('pk')
+	context = {'object_list': object_list}
 
 	return render(request, 'testapp/celery_test.html', context)
 
