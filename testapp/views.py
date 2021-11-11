852ff9aa41ba1619b197f44e453e429e6ccf433c
ksmmr <singo01052@icloud.com>
2021-11-11 17:07:26 +0900

    result.id

--- testapp/views.py
+++ testapp/views.py
@@ -99,6 +99,7 @@ def do_something(request):
         gen = int(request.POST.get("gen"))
         #result = slow_function(number1,number2,gen,set_hikisuu(progress_pk))
         result = slow_function.delay(number1,number2,gen,set_hikisuu(progress_pk))
+        result = result.id
         return render(request, "testapp/result.html", {"result": result})
     else:
         # progress_pkが指定されていない場合の処理
