46bb421858f70c677bc1efa31c099b0d44bb4dae
ksmmr <singo01052@icloud.com>
2021-11-11 17:47:59 +0900

    bug fixed2

--- testapp/views.py
+++ testapp/views.py
@@ -100,9 +100,9 @@ def show_status(request):
     if "task_id" in request.POST:
         task_id = request.POST.get("task_id")
         task1 = AsyncResult(task_id)
-        return render(request, "testapp/result.html", {"status":task1.status})
+        print(task1.status)
+        return render(request, "testapp/result.html", {"result":task1.status})
     else:
-        # progress_pkが指定されていない場合の処理
         return HttpResponse("show_statusエラー")
 #------------------------------------------------------------------------------------------
 
